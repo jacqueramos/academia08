cd459e46f1ee26115b47551ea07ef69435327f18
jacqueramos <jackpotrss501@gmail.com>
2019-11-06 21:06:57 -0300

    utilizando for e while

---
+++ aula06/js/script.js
@@ -0,0 +1,26 @@
+/* while(condição)
+    for(inicio(declaro minha variável dentro do for) ; condiçao ; fim)
+*/
+
+
+/*
+-->while
+
+let animes = ['naruto' , 'boku no hero' , 'kokoro connect' , 'btooom' , 'death note'];
+let i = 0;
+
+while ( i < animes.length)
+{
+    console.log(animes[i]);
+    i++;
+}
+*/
+
+
+//for
+let animes = ['naruto' , 'boku no hero' , 'kokoro connect' , 'btooom' , 'death note'];
+
+for (let e= 0; e < animes.length; e++)
+{
+    console.log(animes[e]);
+}
